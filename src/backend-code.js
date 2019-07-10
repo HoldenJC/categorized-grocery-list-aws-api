3e180854080177d61b45e76ca4d0e08404d2b588
Holden Clark and Saswati Patra <holden@>
2019-07-10 13:43:19 -0700

    add backend-code.js

--- src/backend-code.js
+++ src/backend-code.js
@@ -1,11 +1,21 @@
-// export function delete() {
+// export function addItem(groceryList){
+//   groceryList.push()
+//   groceryList.push({name:`${input.value}`,id:`${idCount}`});
+//   localStorage.setItem('items', JSON.stringify(groceryList));
 //
+// }
+//
+// export function editItem(groceryList){
 //
 //
 // }
 //
-// export function modify() {
+// export function deleteItem(groceryList){
+//   this.
 //
+// }
 //
-//
+// export function clearList(){
+//   localStorage.clear();
+//   $("#groceryList").empty();
 // }
